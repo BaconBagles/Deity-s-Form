710a3c9d742fe79bd2632478733f18bb80adc3a1
Michael <michaelblankdunne@gmail.com>
2021-01-19 17:19:10 +0000

    Very little progress made

--- Initial Project/Assets/projectileScript.cs
+++ Initial Project/Assets/projectileScript.cs
@@ -21,6 +21,7 @@ public class projectileScript : MonoBehaviour
         Vector2 targetPos = target.position;
         Direction = targetPos - (Vector2)transform.position;
         GetComponent<Rigidbody2D>().AddForce(Direction * force);
+        StartCoroutine(SelfDestruct());
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -30,6 +31,13 @@ public class projectileScript : MonoBehaviour
             Audio.Play("PlayerDamage");
             player.GetComponent<PlayerController>().health --; 
         }
+
+        Destroy(this.gameObject);
+    }
+
+    IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(1f);
 
         Destroy(this.gameObject);
     }
