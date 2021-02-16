751c3f83024a5b944bf128a601d12171976f77d1
dinmaker <37370379+dinmaker@users.noreply.github.com>
2021-02-16 11:28:02 +0000

    Game win Scene

--- Initial Project/Assets/Scripts/door.cs
+++ Initial Project/Assets/Scripts/door.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class door : MonoBehaviour
 {
+    GameController gCont;
+
+    void Start()
+    {
+        gCont = FindObjectOfType<GameController>();
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -13,21 +21,30 @@ public class door : MonoBehaviour
             collider.isTrigger = false;
             renderer.enabled = false;
             GameController gCont = FindObjectOfType<GameController>();
-            gCont.currentRoom++;
-            gCont.roomComplete = false;
-            gCont.RandomRoom();
-            gCont.pickupSpawned = false;
-            collision.gameObject.transform.position = new Vector2(0, 0);
-            PlayerController player = collision.GetComponent<PlayerController>();
-            if (player.tempFormActive == true)
+
+            if (gCont.bossRoom == false)
             {
-                player.superForm = false;
-                player.powerAttack = false;
-                player.tempFormActive = false;
+                gCont.currentRoom++;
+                gCont.roomComplete = false;
+                gCont.RandomRoom();
+                gCont.pickupSpawned = false;
+                collision.gameObject.transform.position = new Vector2(0, 0);
+                PlayerController player = collision.GetComponent<PlayerController>();
+                if (player.tempFormActive == true)
+                {
+                    player.superForm = false;
+                    player.powerAttack = false;
+                    player.tempFormActive = false;
+                }
+                if (player.superForm == true || player.powerAttack == true)
+                {
+                    player.tempFormActive = true;
+                }
             }
-            if (player.superForm == true || player.powerAttack == true)
+
+            if (gCont.bossRoom == true)
             {
-                player.tempFormActive = true;
+                SceneManager.LoadScene(3);
             }
         }
     }
