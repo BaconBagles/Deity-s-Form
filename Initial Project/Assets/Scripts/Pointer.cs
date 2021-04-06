4f26491b5b7bc79e35ae6441939cdb47a0db74df
dinmaker <37370379+dinmaker@users.noreply.github.com>
2021-04-06 11:07:23 +0100

    pointer fixed

---
+++ Initial Project/Assets/Scripts/Pointer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    public GameObject player;
+    public GameController gCont;
+    public GameObject door;
+    public GameObject pickup;
+    public Vector3 pointTo;
+    Vector3 pointFrom;
+
+    public bool isPickup;
+    public bool isDoor;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        pickup = gCont.eSpawn;
+        door = gCont.door;
+
+        if (isPickup == true)
+        {
+            pointTo = pickup.transform.position;
+        }
+        if (isDoor == true)
+        {
+            pointTo = door.transform.position;
+        }
+        transform.up = (pointTo - transform.position).normalized;
+    }
+
+}
