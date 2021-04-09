c7e65fb4a4a071b162d3d2721770b52b52d59b90
Michael <michaelblankdunne@gmail.com>
2021-04-09 17:35:49 +0100

    Save system building blocks in

---
+++ Initial Project/Assets/Scripts/Saving/PlayerData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    // List all variables here, needs to be int, float, bool or string
+    //variable
+
+    public PlayerData ( /*all put all needed variable scripts here */)
+    {
+        //variable = script.variable;
+    }
+}
---
+++ Initial Project/Assets/Scripts/Saving/SaveSystem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    public static void SaveGame (/*data scripts */ )
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/Save.binary";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(/*data scripts*/);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadGame()
+    {
+        string path = Application.persistentDataPath + "/Save.binary";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save File not found in " + path);
+            return null;
+        }
+    }
+}
+
