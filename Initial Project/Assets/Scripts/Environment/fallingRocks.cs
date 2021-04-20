b2b89172b067df9077bb1b2526583802a5c977b2
Michael <michaelblankdunne@gmail.com>
2021-04-20 21:59:38 +0100

    Falling Rocks Mfer!

---
+++ Initial Project/Assets/Scripts/Environment/fallingRocks.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingRocks : MonoBehaviour
+{
+     CircleCollider2D mycollider;
+    public GameObject landEffect;
+    Enemy enemy;
+
+    void Start()
+    {
+        mycollider = GetComponent<CircleCollider2D>();
+        mycollider.enabled = false;
+        StartCoroutine(ScaleUp(2f));
+    }
+
+
+    public IEnumerator ScaleUp(float time)
+    {
+        float i = 0;
+        float rate = 1 / time;
+
+        Vector3 oldScale = transform.localScale;
+        Vector3 newScale = new Vector3(10, 10, 0);
+
+        while (i < 1)
+        {
+            i += Time.deltaTime * rate;
+            transform.localScale = Vector3.Lerp(oldScale, newScale, i);
+            yield return 0;
+        }
+
+        mycollider.enabled = true;
+        GameObject effect = Instantiate(landEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 0.5f);
+        CameraShake cam = FindObjectOfType<CameraShake>();
+        cam.StartCoroutine(cam.Shake(0.5f, 1.5f));
+        Destroy(this.gameObject, 0.5f);
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerController player = PlayerController.FindObjectOfType<PlayerController>();
+            player.health -= 4;
+        }
+        else
+        {
+            enemy = collision.gameObject.GetComponent<Enemy>();
+            enemy.health -= 5;
+        }
+    }
+
+}
---
+++ Initial Project/Assets/Scripts/Environment/theStalactiteScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class theStalactiteScript : MonoBehaviour
+{
+    public thePillarScript pillar;
+    public GameObject stalactite;
+    public GameObject[] list;
+    bool triggered;
+    Vector2 pillarposition;
+    Vector2 spawnposition;
+
+    void Start()
+    {
+        pillar = GetComponent<thePillarScript>();
+        pillarposition = transform.position;
+
+        list = new GameObject[Random.Range(1, 6)];
+        triggered = false;
+    }
+
+    void Update()
+    {
+        if (pillar.pillarState == 0 && triggered == false)
+        {
+            for(int i = 0; i < list.Length; i++)
+            {
+                spawnposition = pillarposition + Random.insideUnitCircle * 5;
+                GameObject clone = (GameObject)Instantiate(stalactite, spawnposition, Quaternion.identity);
+
+                list[i] = clone;
+            }
+            triggered = true;
+        }
+
+        if( pillar.pillarState == 3)
+        {
+            triggered = false;
+        }
+    }
+}
