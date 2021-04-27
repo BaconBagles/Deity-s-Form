aa48bf9b8475f889554e6b0ab240557e52880122
Zeppelin03 <Shane.moloney@outlook.ie>
2021-04-27 16:22:11 +0100

    Level 3 Map Additions

---
+++ Initial Project/Assets/WallMarkings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMarkings : MonoBehaviour
+{
+
+    public GameObject currentGameObject;
+    public float alpha = 0.5f;
+    void Start()
+    {
+        currentGameObject = gameObject;
+    }
+
+    void Update()
+    {
+        ChangeAlpha(currentGameObject.GetComponent<Renderer>().material, alpha);
+    }
+
+    void ChangeAlpha(Material mat, float alphaVal)
+    {
+        Color oldColor = mat.color;
+        Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b, alphaVal);
+        mat.SetColor("_Color", newColor);
+
+    }
+}
