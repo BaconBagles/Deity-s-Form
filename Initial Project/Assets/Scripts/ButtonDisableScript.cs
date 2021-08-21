dbb4108ae784210512a1c033edb2c20b9116a141
GraKnight <32521524+GraKnightGames@users.noreply.github.com>
2021-08-21 13:12:14 +0100

    Set up all functionality for new capture screen except returning to base menu

---
+++ Initial Project/Assets/Scripts/ButtonDisableScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonDisableScript : MonoBehaviour
+{
+    [SerializeField] private GameObject confirmScreen;
+    private Button thisButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisButton = this.gameObject.GetComponent<Button>();
+        thisButton.enabled = true;
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (confirmScreen == null)
+        {
+            thisButton.enabled = true;
+        }
+        else if(confirmScreen != null)
+        {
+            thisButton.enabled = false;
+        }
+        confirmScreen = GameObject.FindGameObjectWithTag("AreYouSure");
+    }
+}
